55dffbdf0ada33b1a9b61a2d78073206587767a9
Sachin Gavali <sachingavali321@gmail.com>
2024-01-11 05:05:05 +0530

    setup tailwind css
    tailwind css add to the project

--- src/services/auth/forms/SignUp.jsx
+++ src/services/auth/forms/SignUp.jsx
@@ -2,10 +2,9 @@
 
 const SignUp = () => {
     return (
-        <div>SignUp
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum accusantium ipsa voluptate perspiciatis architecto labore repellat inventore laudantium eveniet. Placeat est non libero eos porro ratione ad repudiandae optio sequi!
-            </p></div>
+        <>
+
+        </>
     )
 }
 
