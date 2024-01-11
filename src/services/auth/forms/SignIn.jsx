765bcc291464a120d2395bfba6ef41e2c35a3124
Sachin Gavali <sachingavali321@gmail.com>
2024-01-12 04:10:24 +0530

    update signup

--- src/services/auth/forms/SignIn.jsx
+++ src/services/auth/forms/SignIn.jsx
@@ -2,7 +2,9 @@
 
 const SignIn = () => {
     return (
-        <div>SignIn</div>
+        <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium, pariatur consequuntur dignissimos voluptates itaque voluptatum labore cupiditate facere veniam minima sequi beatae adipisci quo quam officiis culpa, eius iusto.
+        </div>
     )
 }
 
